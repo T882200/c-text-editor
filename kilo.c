3f626b1f70edf16c9d51484226bf185c34290615
Idan Madar <t882200@gmail.com>
2019-03-18 09:23:49 +0000

    Turn off echoing

--- kilo.c
+++ kilo.c
@@ -1,9 +1,34 @@
-// unistd.h is the name of the header file that provides access to the POSIX operating system API.
+// The <termios.h> header contains the definitions used by the terminal I/O interfaces
+#include <termios.h>
+// unistd.h is the name of the header file that provides access to the POSIX operating system API
 #include <unistd.h>
+
+
+// turn off "echo" mode, (The ECHO feature causes each key you type to be printed to the terminal, so you can see what you’re typing, and by this function below it disabled)
+void enableRawMode(){
+  struct termios raw;
+    
+    tcgetattr(STDIN_FILENO, &raw);
+    // The c_lflag field is for “local flags” | ECHO is a bitflag, defined as 00000000000000000000000000001000 in binary. We use the bitwise-NOT operator (~) on this value to get the opposite to disable the echo mode
+    raw.c_lflag &= ~(ECHO);
+    
+    // The TCSAFLUSH argument specifies when to apply the change: in this case, it waits for all pending output to be written to the terminal, and also discards any input that hasn’t been read.
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+
+
+
+
+
 
 
 // main entry to program
 int main(){
+    
+    enableRawMode();
+    
     // init variable c
     char c;
     
